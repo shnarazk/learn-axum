b7c379e3751ba653d5096ab902d1ca40910544df
Narazaki, Shuji <shujinarazaki@protonmail.com>
2022-01-29 08:27:41 +0900

    https://docs.rs/axum/latest/axum/#routing

--- src/main.rs
+++ src/main.rs
@@ -6,9 +6,29 @@ use axum::{
 #[tokio::main]
 async fn main() {
     let app = Router::new()
-        .route("/", get(|| async { "Hello, World!"}));
+        .route("/", get(root))
+        .route("/", get(get_foo).post(post_foo))
+        .route("/", get(get_foo_bar))
+        ;
     axum::Server::bind(&"0.0.0.0:3000".parse().unwrap())
         .serve(app.into_make_service())
         .await
         .unwrap();
 }
+
+async fn root() -> &'static str {
+    "Hello, World from root!"
+}
+
+async fn get_foo() -> &'static str {
+    "Hello, World from GET foo!"
+}
+
+async fn post_foo() -> &'static str {
+    "Hello, World from POST foo!"
+}
+
+async fn get_foo_bar() -> &'static str {
+    "Hello, World from GET foo/bar!"
+}
+
