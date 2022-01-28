716ee5236f80cb111e310be23cc7ea223dcfc2d8
Narazaki, Shuji <shujinarazaki@protonmail.com>
2022-01-29 08:36:06 +0900

    https://docs.rs/axum/latest/axum/#extractors

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,21 @@
-use axum::{
-    routing::get,
-    Router,
+#![allow(unused)]
+use {
+    axum::{
+        extract::{Json, Path, Query},
+        routing::get,
+        Router,
+    },
+    std::collections::HashMap,
 };
 
 #[tokio::main]
 async fn main() {
     let app = Router::new()
         .route("/", get(root))
-        .route("/", get(get_foo).post(post_foo))
-        .route("/", get(get_foo_bar))
-        ;
+        .route("/foo", get(get_foo).post(post_foo))
+        .route("/foo/bar", get(get_foo_bar))
+        .route("/path", get(path))
+        .route("/query", get(query));
     axum::Server::bind(&"0.0.0.0:3000".parse().unwrap())
         .serve(app.into_make_service())
         .await
@@ -32,3 +38,8 @@ async fn get_foo_bar() -> &'static str {
     "Hello, World from GET foo/bar!"
 }
 
+async fn path(Path(user_id): Path<u32>) {}
+
+async fn query(Query(params): Query<HashMap<String, String>>) {}
+
+async fn json(Json(_payload): Json<serde_json::Value>) {}
