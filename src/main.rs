1ef560bbd4914330f97138e4f585e53bceb271dd
Narazaki, Shuji <shujinarazaki@protonmail.com>
2022-02-03 22:42:42 +0900

    https://docs.rs/axum/latest/axum/#writing-your-own-middleware-with-towerservice

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,20 @@
 use {
     axum::{
-        // body::Body,
+        body::Body,
         extract::{Json, Path, Query},
+        http::Request,
         routing::get,
+        response::Response,
         // response::Json,
         Router,
     },
+    futures::future::BoxFuture,
     serde_json::json,
-    std::collections::HashMap,
+    std::{
+        collections::HashMap,
+        task::{Context, Poll},
+    },
+    tower::{layer::layer_fn, Service},
 };
 
 #[tokio::main]
@@ -19,7 +26,11 @@ async fn main() {
         .route("/json", get(json))
         .route("/path/:id", get(path))
         .route("/query", get(query))
-        .route("/query_json", get(query_json));
+        .route("/query_json", get(query_json))
+        .route("/m", get(|| async { dbg!(); }))
+        .layer(layer_fn(|inner| MyMiddleware { inner }))
+        ;
+        
     axum::Server::bind(&"0.0.0.0:3000".parse().unwrap())
         .serve(app.into_make_service())
         .await
@@ -58,4 +69,35 @@ async fn query(Query(params): Query<HashMap<String, String>>) -> &'static str {
 
 async fn query_json(Json(_payload): Json<serde_json::Value>) -> Json<serde_json::Value> {
     Json(json!({ "data": 42 }))
+}
+
+#[derive(Clone, Debug)]
+struct MyMiddleware<S> {
+    inner: S,
+}
+
+impl<S> Service<Request<Body>> for MyMiddleware<S>
+where
+    S: Service<Request<Body>, Response = Response> + Clone + Send + 'static,
+    S::Future: Send + 'static,
+{
+    type Response = S::Response;
+    type Error = S::Error;
+    type Future = BoxFuture<'static, Result<Self::Response, Self::Error>>;
+
+    fn poll_ready(&mut self, cx: &mut Context<'_>) -> Poll<Result<(), Self::Error>> {
+        self.inner.poll_ready(cx)
+    }
+    fn call(&mut self, req: Request<Body>) -> Self::Future {
+        println!("`MyMiddleware` called!");
+
+        let clone = self.inner.clone();
+        let mut inner = std::mem::replace(&mut self.inner, clone);
+
+        Box::pin(async move {
+            let res: Response = inner.call(req).await?;
+            println!("`MyMiddleware` received the response");
+            Ok(res)
+        })
+    }
 }
