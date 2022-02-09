186cdaf5bdcb60c43cbf02fea6bd5f8f1fa109f6
Narazaki, Shuji <shujinarazaki@protonmail.com>
2022-02-09 18:14:55 +0900

    https://docs.rs/axum/latest/axum/#sharing-state-with-handlers

--- src/bin/client.rs
+++ src/bin/client.rs
@@ -7,16 +7,18 @@ use {
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
-    let json = json!({ "id": 1 });
-    let request = Request::builder()
-        .method(Method::POST)
-        .uri(Uri::from_str(&format!("http://{}/query_json", PORT))?)
-        .header("Content-Type", HeaderValue::from_static("application/json"))
-        .body(Body::from(json.to_string()))
-        .unwrap();
-    let res = Client::new().request(request).await?;
-    let status = res.status();
-    let buf = hyper::body::to_bytes(res).await?;
-    println!("got status: {}, body: {buf:?}", status);
+    for _ in 0..2 {
+        let json = json!({ "id": 1 });
+        let request = Request::builder()
+            .method(Method::POST)
+            .uri(Uri::from_str(&format!("http://{}/query_json", PORT))?)
+            .header("Content-Type", HeaderValue::from_static("application/json"))
+            .body(Body::from(json.to_string()))
+            .unwrap();
+        let res = Client::new().request(request).await?;
+        let status = res.status();
+        let buf = hyper::body::to_bytes(res).await?;
+        println!("got status: {}, body: {buf:?}", status);
+    }
     Ok(())
 }
--- src/bin/server.rs
+++ src/bin/server.rs
@@ -1,28 +1,53 @@
 use {
     axum::{
-        extract::{Json, Path, Query},
+        extract::{Json, Path, Query}, // Extension,
         routing::{get, post},
+        AddExtensionLayer,
         Router,
     },
     learn_axum::{logging::LogService, sample_middle::MyMiddleware, PORT},
+    serde::Deserialize,
     serde_json::json,
-    std::collections::HashMap,
+    std::{
+        collections::HashMap,
+        sync::{Arc, Mutex},
+    },
     tower::layer::layer_fn,
 };
+
+#[derive(Debug, Default, Deserialize)]
+struct State {
+    count: usize,
+}
 
 #[tokio::main]
 async fn main() {
     console_subscriber::init();
+    let shared_state = Arc::new(Mutex::new(State::default()));
     let app = Router::new()
         .route("/", get(root))
         .route("/foo", get(get_foo).post(post_foo))
         .route("/foo/bar", get(get_foo_bar))
         .route("/json", get(json))
         .route("/path/:id", get(path))
-        .route("/query", get(query))
-        .route("/query_json", post(query_json))
+        .route(
+            "/query",
+            get({
+                let shared_state = Arc::clone(&shared_state);
+                move |body| query(body, Arc::clone(&shared_state))
+            }),
+        )
+        .route(
+            "/query_json",
+            post({
+                let shared_state = Arc::clone(&shared_state);
+                move |body| query_json(body, Arc::clone(&shared_state))
+            }),
+        )
         .layer(layer_fn(|service| LogService::new(service, "test")))
-        .layer(layer_fn(MyMiddleware::new));
+        .layer(layer_fn(MyMiddleware::new))
+        .layer(AddExtensionLayer::new(shared_state));
+
     axum::Server::bind(&PORT.parse().unwrap())
         .serve(app.into_make_service())
         .await
@@ -54,12 +79,20 @@ async fn path(Path(user_id): Path<u32>) -> &'static str {
     "ok"
 }
 
-async fn query(Query(params): Query<HashMap<String, String>>) -> &'static str {
+async fn query(
+    Query(params): Query<HashMap<String, String>>,
+    _state: Arc<Mutex<State>>,
+) -> &'static str {
     dbg!(params);
     "this is query\n"
 }
 
-async fn query_json(Json(payload): Json<serde_json::Value>) -> Json<serde_json::Value> {
+async fn query_json(
+    Json(payload): Json<serde_json::Value>,
+    state: Arc<Mutex<State>>,
+) -> Json<serde_json::Value> {
     dbg!(payload);
-    Json(json!({ "data": 42 }))
+    let mut data = state.lock().unwrap();
+    data.count += 1;
+    Json(json!({ "data": data.count }))
 }
