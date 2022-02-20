782d53905d81bbad2f8785b917391a8eda6ef8b4
Narazaki, Shuji <shujinarazaki@protonmail.com>
2022-02-20 23:21:12 +0900

    done the former part of Tokio Tutorial Spawning

--- src/bin/redis.rs
+++ src/bin/redis.rs
@@ -1,35 +1,54 @@
 use {
+    axum::body::Bytes,
     mini_redis::{Connection, Frame, Result},
+    std::{collections::HashMap, sync::{Arc, Mutex}},
     tokio::net::{TcpListener, TcpStream},
 };
+
+type Db = Arc<Mutex<HashMap<String, Bytes>>>;
 
 #[tokio::main]
 async fn main() -> Result<()> {
     let listener = TcpListener::bind("127.0.0.1:6379").await.unwrap();
+    println!("Listening...");
+
+    // On using std::sync::Mutex
+    // 
+    // Note, std::sync::Mutex and not tokio::sync::Mutex is used to guard the HashMap. A common
+    // error is to unconditionally use tokio::sync::Mutex from within async code. An async mutex is
+    // a mutex that is locked across calls to .await. 
+    // 
+    // A synchronous mutex will block the current thread when waiting to acquire the lock. This, in
+    // turn, will block other tasks from processing. However, switching to tokio::sync::Mutex
+    // usually does not help as the asynchronous mutex uses a synchronous mutex internally. 
+    // 
+    // As a rule of thumb, using a synchronous mutex from within asynchronous code is fine as long
+    // as contention remains low and the lock is not held across calls to .await. Additionally,
+    // consider using parking_lot::Mutex as a faster alternative to std::sync::Mutex. 
+    let db: Db = Arc::new(Mutex::new(HashMap::new()));
     loop {
         let (socket, _) = listener.accept().await.unwrap();
-        process(socket).await;
+        let db = db.clone();
+        print!("Accepted");
+        tokio::spawn(async move {process(socket, db).await;});
     }
-    // client.set("hello", "world".into()).await?;
-    // let result = client.get("hello").await?;
-    // println!("got value from the server; result={:?}", result);
-    // Ok(())
 }
 
-async fn process(socket: TcpStream) {
+async fn process(socket: TcpStream, db: Db) {
     use mini_redis::Command::{self, Get, Set};
-    use std::collections::HashMap;
-    let mut db = HashMap::new();
+    // let mut db = HashMap::new();
     let mut connection = Connection::new(socket);
     while let Some(frame) = connection.read_frame().await.unwrap() {
         let response = match Command::from_frame(frame).unwrap() {
             Set(cmd) => {
-                db.insert(cmd.key().to_string(), cmd.value().to_vec());
+                let mut db = db.lock().unwrap();
+                db.insert(cmd.key().to_string(), cmd.value().clone());
                 Frame::Simple("OK".to_string())
             }
             Get(cmd) => {
+                let db = db.lock().unwrap();
                 if let Some(value) = db.get(cmd.key()) {
-                    Frame::Bulk(value.clone().into())
+                    Frame::Bulk(value.clone())
                 } else {
                     Frame::Null
                 }
