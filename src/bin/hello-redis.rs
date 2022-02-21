b41369e98bdc4cc7c8e17f6bdf45fe5a28dd1ec1
Narazaki, Shuji <shujinarazaki@protonmail.com>
2022-02-21 12:51:55 +0900

    done the former part of Tokio Tutorial Channels

--- src/bin/hello-redis.rs
+++ src/bin/hello-redis.rs
@@ -1,7 +1,74 @@
-use mini_redis::{client, Result};
+use {
+    bytes::Bytes,
+    mini_redis::{client, Result},
+    tokio::sync::{mpsc, oneshot},
+};
+
+#[derive(Debug)]
+enum Command {
+    Get {
+        key: String,
+        resp: Responder<Option<Bytes>>,
+    },
+    Set {
+        key: String,
+        val: Bytes,
+        resp: Responder<()>,
+    },
+}
+
+type Responder<T> = oneshot::Sender<mini_redis::Result<T>>;
 
 #[tokio::main]
-async fn main() -> Result<()> {
+async fn main() {
+    let (tx, mut rx) = mpsc::channel(32);
+    let tx2 = tx.clone();
+
+    let t1 = tokio::spawn(async move {
+        let (resp_tx, resp_rx) = oneshot::channel();
+        let cmd = Command::Get {
+            key: "hello".to_string(),
+            resp: resp_tx,
+        };
+        // "sending from first handle"
+        tx.send(cmd).await.unwrap();
+        let res = resp_rx.await;
+        println!("GOT = {:?}", res);
+    });
+    let t2 = tokio::spawn(async move {
+        let (resp_tx, resp_rx) = oneshot::channel();
+        let cmd = Command::Set {
+            key: "foo".to_string(),
+            val: "bar".into(),
+            resp: resp_tx,
+        };
+        tx2.send(cmd).await.unwrap();
+        let res = resp_rx.await;
+        println!("GOT = {:?}", res);
+    });
+    let manager = tokio::spawn(async move {
+        let mut client = client::connect("127.0.0.1:6379").await.unwrap();
+        while let Some(cmd) = rx.recv().await {
+            match cmd {
+                Command::Get { key, resp } => {
+                    let res = client.get(&key).await;
+                    let _ = resp.send(res);
+                }
+                Command::Set { key, val, resp } => {
+                    let res = client.set(&key, val).await;
+                    let _ = resp.send(res);
+                }
+            }
+        }
+    });
+    t1.await.unwrap();
+    t2.await.unwrap();
+    manager.await.unwrap();
+}
+
+// #[tokio::main]
+#[allow(dead_code)]
+async fn _main() -> Result<()> {
     let mut client = client::connect("127.0.0.1:6379").await?;
     client.set("hello", "world".into()).await?;
     let result = client.get("hello").await?;
