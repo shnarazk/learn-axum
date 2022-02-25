aeef02ec38d801e128289c6be1c7bfe809d2cf7d
Narazaki, Shuji <shujinarazaki@protonmail.com>
2022-02-25 21:00:11 +0900

    fukuoka.rs: tiny change

--- src/bin/fukuoka.rs
+++ src/bin/fukuoka.rs
@@ -2,23 +2,20 @@ use {hyper::Client, hyper_tls::HttpsConnector, regex::Regex};
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
-    {
-        let base =
-            "https://ckan.open-governmentdata.org/dataset/401000_pref_fukuoka_covid19_patients";
-        let target = Regex::new("https://ckan[^\"]+csv").expect("wrong regex");
-        let https = HttpsConnector::new();
-        let client = Client::builder().build::<_, hyper::Body>(https);
-        let res = client.get(base.parse()?).await?;
-        let buf: axum::body::Bytes = hyper::body::to_bytes(res).await?;
-        let str = String::from_utf8_lossy(buf.as_ref());
-        for l in str.lines() {
-            if let Some(rurl) = target.captures(l) {
-                let url = &rurl[0];
-                let res = client.get(url.parse()?).await?;
-                let buf = hyper::body::to_bytes(res).await?;
-                let str = String::from_utf8_lossy(buf.as_ref());
-                print!("{str}");
-            }
+    let base = "https://ckan.open-governmentdata.org/dataset/401000_pref_fukuoka_covid19_patients";
+    let target = Regex::new("https://ckan[^\"]+csv").expect("wrong regex");
+    let https = HttpsConnector::new();
+    let client = Client::builder().build::<_, hyper::Body>(https);
+    let res = client.get(base.parse()?).await?;
+    let buf: axum::body::Bytes = hyper::body::to_bytes(res).await?;
+    let str = String::from_utf8_lossy(buf.as_ref());
+    for l in str.lines() {
+        if let Some(rurl) = target.captures(l) {
+            let url = &rurl[0];
+            let res = client.get(url.parse()?).await?;
+            let buf = hyper::body::to_bytes(res).await?;
+            let str = String::from_utf8_lossy(buf.as_ref());
+            print!("{str}");
         }
     }
     Ok(())
